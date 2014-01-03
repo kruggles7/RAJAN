d65876e955536e6bf94af663b15476ee31f42cfd
Kelly Ruggles <kelly.ruggles@nyumc.org>
2014-01-02 19:10:33 -0500

    trend analysis

--- relative _risk/trend_analysis_all.m
+++ relative _risk/trend_analysis_all.m
@@ -1,12 +1,24 @@
 %make heatmaps out of the relative risk matrix 
 xlab={'2001', '2003', '2005', '2007', '2009', '2011'}; 
-% cd matrices
-% load rel_risk_RC_111413.mat
-% load q_labels.mat
-% cd ..
+cd matrices
+load rel_risk_RC_111413.mat
+load q_labels.mat
+cd ..
 
 trends=double.empty; 
 count=1; 
+plot1=double.empty; 
+plot2=double.empty; 
+plot3=double.empty; 
+plot4=double.empty; 
+cell1=cell.empty; 
+cell2=cell.empty; 
+cell3=cell.empty; 
+cell4=cell.empty; 
+c1=1; 
+c2=1; 
+c3=1; 
+c4=1; 
 for i=1:80
     i_char=num2str(i); 
     q1_=i_char; 
@@ -69,12 +81,158 @@ for i=1:80
         n=1; 
         trends(:,count)=d; %NEED TO TAKE OUT ONE OF THE QUESTIONS (56,57)
         count=count+1; 
+        indxu=find(d>50);
+        indxd=find(d<-50); 
+        bar_color=zeros(length(d),1); 
+        bar_color(indxu)=1; 
+        bar_color(indxd)=-1; 
+        if numel(indxu)>0 && numel(indxd)>0
+            mycmap=[0/255 139/255 69/255; 54/255 100/255 139/255; 205/255 51/255 51/255];  
+            group=1; 
+            plot1(c1,:)=d; 
+            indx=find(strcmp(q1,qlabel(:,2))==1); 
+            if numel(indx)>0
+                cell1(c1,:)=qlabel(indx,:); 
+            end
+            c1=c1+1; 
+        elseif numel(indxu)>0
+            mycmap=[54/255 100/255 139/255; 205/255 51/255 51/255];
+            group=2; 
+            plot2(c2,:)=d; 
+            indx=find(strcmp(q1,qlabel(:,2))==1); 
+            if numel(indx)>0
+                cell2(c2,:)=qlabel(indx,:); 
+            end
+            c2=c2+1; 
+        elseif numel(indxd)>0
+            mycmap=[0/255 139/255 69/255; 54/255 100/255 139/255];
+            group=3; 
+            indx=find(strcmp(q1,qlabel(:,2))==1); 
+            if numel(indx)>0
+                cell3(c3,:)=qlabel(indx,:); 
+            end
+            plot3(c3,:)=d; 
+            c3=c3+1; 
+        else 
+            mycmap=[54/255 100/255 139/255]; 
+            group=4; 
+            indx=find(strcmp(q1,qlabel(:,2))==1); 
+            if numel(indx)>0
+                cell4(c4,:)=qlabel(indx,:);  
+            end 
+            plot4(c4,:)=d; 
+            c4=c4+1; 
+        end 
+        bh=bar(d); 
+        ch=get(bh,'Children');  
+        set (ch,'CData',bar_color) ; 
+        %colormap(winter); 
+        colormap(mycmap); 
+        hold on
+        x=[0 length(d)+1];
+        hold off
+        ylim([-150 150]); 
+        %print (gcf,'-depsc2',[q1 '_trends']);
+        print (gcf,'-dpng',[q1 '_trends']);
+        
     end 
 end
 
-cg=clustergram(trends, 'ColorMap', 'jet', 'DisplayRange', 50, 'RowLabels',qlabel(:,1),'ColumnLabels',qlabel(:,1)); 
+%%Changes in both -----------------------------------
+[r,c]=size(plot1); 
+ymax=max(max(abs(plot1))); 
+s=0; 
+for i=1:r
+    F2=subplot(r,1,i); 
+    temp=plot1(i,:); 
+    indxu=find(temp>50);
+    indxd=find(temp<-50); 
+    bar_color=zeros(length(temp),1); 
+    bar_color(indxu)=1; 
+    bar_color(indxd)=-1; 
+    mycmap=[0/255 139/255 69/255; 54/255 100/255 139/255; 205/255 51/255 51/255];  
+    bh=bar(temp); 
+    ch=get(bh,'Children');  
+    set (ch,'CData',bar_color) ; 
+    %colormap(winter); 
+    colormap(mycmap); 
+    hold on
+    x=[0 length(d)+1];
+    hold off
+    ylim([-ymax ymax]); 
+    xlim([0 56]); 
+    if s==1
+        linkaxes([F F2],'y'); %make y axis the same
+        pos1=get(F,'Position'); %find the current position [x,y,width,height]
+        pos2=get(F2,'Position'); %find the current position [x,y,width,height]
+        pos2(2)=pos1(2) - pos2(4); %move the second so it touches the first 
+        set (F2,'Position',pos2); 
+    end 
+    if i==r
+        set (F2, 'YTickLabel', []); 
+        ylabel(cell1{i,2});
+    else 
+        s=1;  
+        set(F2, 'XTickLabel',[]); 
+        set(F2, 'YTickLabel',[]);
+        set (F2, 'XTick', []); 
+        ylabel(cell1{i,2}); 
+        F=F2; 
+    end 
+end 
+hold off 
+print (gcf,'-dpng','trend_changes_both.png');
 
+%only up-----------------------------------------
 
+[r,c]=size(plot2); 
+ymax=max(max(abs(plot2))); 
+s=0; 
+for i=1:r
+    F2=subplot(r,1,i); 
+    temp=plot2(i,:); 
+    indxu=find(temp>50);
+    indxd=find(temp<-50); 
+    bar_color=zeros(length(temp),1); 
+    bar_color(indxu)=1; 
+    bar_color(indxd)=-1; 
+    mycmap=[54/255 100/255 139/255; 205/255 51/255 51/255];
+    bh=bar(temp); 
+    ch=get(bh,'Children');  
+    set (ch,'CData',bar_color) ; 
+    %colormap(winter); 
+    colormap(mycmap); 
+    hold on
+    x=[0 length(d)+1];
+    hold off
+    ylim([-ymax ymax]); 
+    xlim([0 56]); 
+    if s==1
+        linkaxes([F F2],'x'); %make y axis the same
+        pos1=get(F,'Position') %find the current position [x,y,width,height]
+        pos2=get(F2,'Position') %find the current position [x,y,width,height]
+        %pos2(2)=pos1(2) - pos2(4); %move the second so it touches the first 
+       % set (F2,'Position',pos2); 
+    end 
+    if i==r
+        set (F2, 'YTickLabel', []); 
+        ylabel(cell2{i,2});
+    else 
+        s=1;  
+        set(F2, 'XTickLabel',[]); 
+        set(F2, 'YTickLabel',[]);
+        set (F2, 'XTick', []); 
+        ylabel(cell2{i,2}); 
+    end 
+    F=F2; 
+end 
+hold off 
+print (gcf,'-dpng','trend_changes_up.png');
+%trends=trends(:,1:55); 
+%cg=clustergram(trends, 'ColorMap', 'jet', 'DisplayRange', 25,
+%'RowLabels',qlabel(:,1),'ColumnLabels',qlabel(:,1),'Cluster',2,'Symmetric','true');  ,'DisplayRange', 10,'Symmetric','true', 'Dendrogram',['' 25],
+%cg=clustergram(trends,'RowLabels',qlabel(:,1),'ColumnLabels',qlabel(:,1),'Cluster',2, 'Colormap','jet','DisplayRange', 5,'Symmetric','false', 'LogTrans', 0 ); 
+%hm=HeatMap(trends ); 
 
 
 
